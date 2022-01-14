9ed66c915d707a1108d934c32a348d16a5242ea2
octo <octovianus.pabubung@helios.id>
2022-01-14 11:15:52 +0700

    second commit

---
+++ precache-manifest.a5aef12d47d88bb3e961a0019e73a6d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f8131f7afe2eb8ebca5d6ea229ba6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81ffba9d031f040a418c",
+    "url": "/static/css/main.a7da208c.chunk.css"
+  },
+  {
+    "revision": "611cd680ef8b8cb36db5",
+    "url": "/static/js/2.0b1d64fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0b1d64fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ffba9d031f040a418c",
+    "url": "/static/js/main.aa2014bd.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  },
+  {
+    "revision": "e803200ac439b7568c087fde966de554",
+    "url": "/static/media/img1.e803200a.jpg"
+  }
+]);
